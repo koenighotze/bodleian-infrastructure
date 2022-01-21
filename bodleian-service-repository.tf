be3a8381ddac41337a8cd135bbc96dacc263e001
David Schmitz <davidschmitz@posteo.de>
2022-01-21 09:36:07 +0100

    Fix formatting

--- bodleian-service-repository.tf
+++ bodleian-service-repository.tf
@@ -1,10 +1,10 @@
 module "bodleian-service-repository" {
-    source = "./modules/repository"
+  source = "./modules/repository"
 
-    target_repository_name = "koenighotze/bodleian"
-    codacy_api_token = var.codacy_api_token
-    docker_registry_username = var.docker_registry_username
-    docker_registry_token = var.docker_registry_token
-    gcp_project_id = var.project_id
-    gcp_sa_private_key = google_service_account_key.cicd_cloudrun_sa_key.private_key
+  target_repository_name   = "koenighotze/bodleian"
+  codacy_api_token         = var.codacy_api_token
+  docker_registry_username = var.docker_registry_username
+  docker_registry_token    = var.docker_registry_token
+  gcp_project_id           = var.project_id
+  gcp_sa_private_key       = google_service_account_key.cicd_cloudrun_sa_key.private_key
 }
--- variables.tf
+++ variables.tf
@@ -1,35 +1,35 @@
 variable "region" {
-  type    = string
-  default = "europe-west1"
+  type        = string
+  default     = "europe-west1"
   description = "The GCP region"
 }
 
 variable "zone" {
-  type    = string
-  default = "europe-west1-c"
+  type        = string
+  default     = "europe-west1-c"
   description = "The GCP zone"
 }
 
 variable "project_id" {
-  type      = string
-  sensitive = true
+  type        = string
+  sensitive   = true
   description = "The GCP project id"
 }
 
 variable "codacy_api_token" {
-  type      = string
-  sensitive = true
+  type        = string
+  sensitive   = true
   description = "The API token for uploading coverage data"
 }
 
 variable "docker_registry_username" {
-  type      = string
-  sensitive = true
+  type        = string
+  sensitive   = true
   description = "The username for uploading images"
 }
 
 variable "docker_registry_token" {
-  type      = string
-  sensitive = true
+  type        = string
+  sensitive   = true
   description = "The API token for uploading images"
 }
