cdabdfb9d1b0a1203a8515a967ef35c55cc483cb
David Schmitz <davidschmitz@posteo.de>
2022-01-24 13:59:33 +0100

    Fix configuration of sa resource

--- modules/deployer-service-account/main.tf
+++ modules/deployer-service-account/main.tf
@@ -4,7 +4,7 @@ locals {
 
 resource "google_service_account" "cicd_cloudrun_sa" {
   project      = var.gcp_project_id
-  account_id   = "cicd-sa-id-${var.name}"
+  account_id   = var.name
   display_name = "Service Account for CICD actions related to cloud run"
 }
 
@@ -12,7 +12,7 @@ resource "google_project_iam_member" "cicd_cloudrun_sa_iam_member" {
   for_each = toset(local.cicd_cloudrun_sa_roles)
 
   role   = each.value
-  member = "cicd-sa-id-${var.name}:${google_service_account.cicd_cloudrun_sa.email}"
+  member = "serviceAccount:${google_service_account.cicd_cloudrun_sa.email}"
 }
 
 resource "google_service_account_key" "cicd_cloudrun_sa_key" {
